10c6b0968a71681651edf22a67e42c797777a5fc
cbonaco1 <christian.bonacore@gmail.com>
2020-02-02 15:34:44 -0500

    Add some basic styling to the login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,8 +3,13 @@ import './Login.scss'
 
 export default () => {
     return (
-        <div className="login">
-            <p>Login</p>
+        <div className="login component">
+            <div className="component-container">
+                <h1 className="title">Join Chat</h1>
+                <p className="prompt">Please enter your username</p>
+                <input type="text" name="username" id="username"/>
+                <button className="submit-btn">Next</button>
+            </div>
         </div>
     )
 }
