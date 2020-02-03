43319e6250f9bfe83f713ceedb7b0268f3cfca3b
Christian Bonacore <christian.bonacore@pager.com>
2020-02-03 09:37:38 -0500

    Basic start to rendering list of chats

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -18,7 +18,9 @@ export default class Chat extends React.Component {
 
         this.socket.on('message', (message) => {
             console.log(message);
-            // this.state.messages.append(message)
+            this.setState({
+                messages: [message, ...this.state.messages]
+            });
         });
     }
 
@@ -36,11 +38,15 @@ export default class Chat extends React.Component {
     }
 
     render() {
-        // iterate through this.state.messages,
-        // create a <ChatItem /> component
+        const messagesList = this.state.messages.map((msg) => <li>{msg.text}</li>);
 
         return (
             <div className="chat">
+                <div>
+                    <ul>
+                        {messagesList}
+                    </ul>
+                </div>
                 <p>Chat page for {this.props.location.state}</p>
                 <input type="text" value={this.state.message} onChange={this.onInputChange.bind(this)}/>
                 <button onClick={this.onSend.bind(this)}>Send</button>
